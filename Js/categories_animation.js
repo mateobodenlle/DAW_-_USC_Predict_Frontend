61c80e6c4f9dfe6b4d7f28cf7f340893182abc38
2cristo7 <diego.cristo27@gmail.com>
2025-04-02 12:56:11 +0200

    Responsividad de tamaño móbil: - Página de búsqueda: se han hecho ajustes en los tamaños de las tarjetas en el modo de búsqueda de tarjetas - Display de categorías: se cambió el comportamiento para que solo se pueda tener 1 abierta a la vez. Se actualizó el comportamiento del botón de ver más.

--- Js/categories_animation.js
+++ Js/categories_animation.js
@@ -12,19 +12,7 @@ export function categoryAnimation (categories_container) {
     // Botón para cambiar la disposición de las tarjetas en móvil
     if (esMovil) {
         const seeMore_button = categories_container.querySelector("#seeMore-button");
-
         seeMore_button.style.display = "none";
-
-        seeMore_button.addEventListener("click", () => {
-            const cardsList = document.querySelectorAll(".cards");
-
-            cardsList.forEach(cards => {
-                cards.classList.toggle("modo-columna");
-            });
-
-            const enModoColumna = cardsList[0].classList.contains("modo-columna");
-            seeMore_button.textContent = enModoColumna ? "See less" : "See all";
-        });
     }
 
     summaries.forEach(summary => {
@@ -33,20 +21,15 @@ export function categoryAnimation (categories_container) {
             const content = summary.nextElementSibling; // Category content
             const isVisible = content.classList.contains("visible");
 
-            // Cerrar todas las categorías
-            if (!esMovil) {
-                categories_container.querySelectorAll(".categoria-content.visible").forEach(c => {
+            // Cerrar todas las categorías al clicar en otra
+            categories_container.querySelectorAll(".categoria-content.visible").forEach(c => {
 
-                    c.classList.remove("visible"); // quitar categoria-content.visible
-                    setTimeout(() => {
-                        c.previousElementSibling.classList.remove("animado"); // quitar animado de categoria-summary, titulo, descripcion y num
-                        c.closest(".categoria").classList.remove("animado"); // quitar categoria.animado
-                    }, delay);
-                });
-            }
-            else
-                content.classList.remove("visible"); // En móvil al seleccionar otra categoría no se cierran las anteriores, dado que el display es vertical y se mueve to_do
-
+                c.classList.remove("visible"); // quitar categoria-content.visible
+                setTimeout(() => {
+                    c.previousElementSibling.classList.remove("animado"); // quitar animado de categoria-summary, titulo, descripcion y num
+                    c.closest(".categoria").classList.remove("animado"); // quitar categoria.animado
+                }, delay);
+            });
 
             // Si no estaba visible, mostrarlo
             if (!isVisible) {
@@ -66,18 +49,32 @@ export function categoryAnimation (categories_container) {
             const tarjetas = content.querySelectorAll(".tarjeta-evento");
             const btnRight = content.querySelector('.scroll-right');
             if (tarjetas.length > 3) {
-
                 content.classList.add("sombra-derecha");
                 if (!esMovil)
                     btnRight.style.display = "flex";
             }
 
-            // Botón para cambiar la disposición de las tarjetas en móvil
+
             if (esMovil) {
                 const seeMore_button = categories_container.querySelector("#seeMore-button");
-                if (seeMore_button && seeMore_button.style) {
-                    seeMore_button.style.display = (seeMore_button.style.display === "none") ? "block" : "none";
+
+                // Acción del botón para cambiar la disposición de las tarjetas en móvil
+                seeMore_button.onclick = function (){
+                    const categoria_content = summary.nextElementSibling;
+                    const cardsList = categoria_content.querySelectorAll(".cards");
+                    cardsList.forEach(cards => {
+                        cards.classList.toggle("modo-columna");
+                        categoria_content.classList.toggle("modo-columna"); // Para quitar las sombras
+                    });
+
+                    const enModoColumna = cardsList[0].classList.contains("modo-columna");
+                    seeMore_button.textContent = enModoColumna ? "See less" : "See all";
                 }
+                // Posición del botón
+                if (seeMore_button && seeMore_button.style) {
+                    seeMore_button.style.display = isVisible? "none" : "block";
+                }
+
             }
 
         });
