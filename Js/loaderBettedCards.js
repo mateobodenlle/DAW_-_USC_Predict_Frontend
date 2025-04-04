e9e7ef94c9f84d79a954a6eaf95e0f9c46f44c09
mateobodenlle <mateobodenlle@gmail.com>
2025-04-04 13:32:50 +0200

    Carga dinámica de apuestas del apartado Watchlist en Watchlist.html

--- Js/loaderBettedCards.js
+++ Js/loaderBettedCards.js
@@ -1,46 +1,38 @@
-function crearTarjetaEvento(evento, onClick, onSi, onNo) {
+export function createBettedCard(betData, onClick) {
     const tarjeta = document.createElement("article");
     tarjeta.className = "tarjeta-evento";
-
     tarjeta.innerHTML = `
-                            <img src="${evento.img}" alt="Evento">
-                            <div class="tarjeta-info">
-                                <h3>${evento.nombre}</h3>
-                                <p>${evento.categoria}</p>
-                            </div>
-                            <div class="tarjeta-porcentajes">
-                                <h3 class="porcentaje">${evento.porcentaje || ''}</h3>
-                                <div class="variacion losing">
-                                
-                                    <svg id="flecha_arriba" class="winning" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                      <path d="M12 4L12 20M12 4L8 8M12 4L16 8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg>
-                                    <svg id="flecha_abajo" class="losing" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                      <path d="M12 20L12 4M12 20L8 16M12 20L16 16" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg>
-
-                                    <h3>${evento.porcentaje || ''}</h3>
-                                </div>
-                            </div>
-                                
-                            <div class="posicion">
-                                <h3>Posición</h3>
-                                <p>230 votos</p>
-                            </div>
-                            
-                            <p id="decision">SI</p>
-                            
-                            <button class="btn-editar">EDITAR</button>
+        <img src="${betData.img}" alt="Evento">
+        <div class="tarjeta-info">
+            <h3>${betData.nombre}</h3>
+            <p>${betData.categoria}</p>
+        </div>
+        <div class="tarjeta-porcentajes">
+            <h3 class="porcentaje">${betData.porcentaje || ''}</h3>
+            <div class="variacion losing">
+                <svg id="flecha_arriba" class="winning" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 4L12 20M12 4L8 8M12 4L16 8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <svg id="flecha_abajo" class="losing" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 20L12 4M12 20L8 16M12 20L16 16" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <h3>${betData.porcentaje || ''}</h3>
+            </div>
+        </div>
+        <div class="posicion">
+            <h3>Posición</h3>
+            <p>230 votos</p>
+        </div>
+        <p id="decision">SI</p>
+        <button class="btn-editar">EDITAR</button>
     `;
-
-    // Para hacer click en toda la tarjeta y redirigir
-    tarjeta.addEventListener("click", () => onClick(evento));
+    tarjeta.addEventListener("click", () => onClick(betData));
     return tarjeta;
 }
 
 export function generateBettedCards(contenedor, eventos, onClick, onSi, onNo) {
     eventos.forEach(evento => {
-        const tarjeta = crearTarjetaEvento(evento, onClick, onSi, onNo);
-        contenedor.appendChild(tarjeta);
+        const tarjeta = createBettedCard(evento, onClick);
+        $(contenedor).append(tarjeta);
     });
 }
